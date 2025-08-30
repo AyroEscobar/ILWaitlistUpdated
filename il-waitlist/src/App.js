ec8639acdc686db050ff16be47058084618d37e3
Ayro Escobar <ayro.escobar@gmail.com>
2025-08-29 23:30:11 -0500

    Adding supabase keys and fixing vite logic

--- il-waitlist/src/App.js
+++ il-waitlist/src/App.js
@@ -1,15 +1,17 @@
+
+import React from 'react';
 import { createClient } from '@supabase/supabase-js';
 
-const url = 'https://tqsjtxyzbdvgjatvochr.supabase.co';
-const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxc2p0eHl6YmR2Z2phdHZvY2hyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMTU0MjEsImV4cCI6MjA2NTg5MTQyMX0.1cU8UxeHncWQPNXqn5dtGR7A6AO7pKwQiW-_77tUWdI';
+
+const url = process.env.REACT_APP_SUPABASE_URL;
+const key = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 const supabase = createClient(url, key);
 
 function App() {
   return (
-    <div>
-      <p>URL: {url}</p>
-      <p>Key: {key}</p>
+     <div>
+    <h1>where are you going to be</h1>
     </div>
   );
 }
